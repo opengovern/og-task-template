6f5daf70c17734c3d0629269deac68bd1fae8322
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2024-12-09 23:41:27 +0330

    feat: add grpc instead of nats to template

--- sender/main.go
+++ sender/main.go
@@ -5,8 +5,14 @@ import (
 	"encoding/json"
 	"log"
 	"os"
+	"time"
+
 	"github.com/nats-io/nats.go/jetstream"
 	"github.com/opengovern/og-util/pkg/jq"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/protobuf/runtime/protoimpl"
 
 	"go.uber.org/zap"
 )
@@ -24,6 +30,8 @@ var (
 	NatsURL = os.Getenv("NATS_URL")
 	// Topic name Sender
 	TopicNameSender = os.Getenv("TOPIC_NAME_SENDER")
+	// GRPC server url
+	GRPCServerURL = os.Getenv("GRPC_SERVER_URL")
 
 )
 
@@ -68,7 +76,7 @@ func main() {
 				log.Fatalf("failed to read file: %v", err)
 			}
 			// send the result to the topic
-			_,err = jqClient.Produce(ctx, TopicNameSender, data[:count], TaskId)
+			err = sendGRPC(ctx,data[:count],logger)
 			if err != nil {
 				logger.Error("Failed to publish ComplianceReportJob results", zap.Error(err))
 			}
@@ -81,4 +89,60 @@ func main() {
 	}
 	
 }
+
+// function to send data with grpc
+type ResponseOK struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+}
+func sendGRPC(ctx context.Context,data []byte, logger *zap.Logger) error {
+	// create a grpc connection
+	// send the data to the grpc server
+	// the grpc server will save the data to the database
+	// create a grpc connection
+	grpcCtx := metadata.NewOutgoingContext(ctx, metadata.New(map[string]string{}))
+	var client grpc.ClientConnInterface
+
+	logger.Info("Setting grpc connection opts")
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	for retry := 0; retry < 5; retry++ {
+		conn, err := grpc.NewClient(
+			GRPCServerURL,
+			opts...,
+		)
+		if err != nil {
+			logger.Error("[result delivery] connection failure:", zap.Error(err))
+			if retry == 4 {
+				return err
+			}
+			time.Sleep(1 * time.Second)
+			continue
+		}
+		client = conn
+
+		break
+	}
+	for retry := 0; retry < 5; retry++ {
+		// send the data to the grpc server
+		out := new(ResponseOK)
+		 err := client.Invoke(grpcCtx, "/Tasks/Results", data,out)
+		if err != nil {
+			logger.Error("[result delivery] failed to send result:", zap.Error(err))
+		}
+			if retry== 4 {
+				return err
+			}
+			time.Sleep(1 * time.Second)
+			continue
+		}
+
+	return nil
+
+
+
+
+}
+
 
